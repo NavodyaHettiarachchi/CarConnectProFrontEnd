2b3eb107a849934711874519ba3e9f501024dcc0
Sandun Subhashana <sandunsub2560@gmail.com>
2024-01-18 17:57:40 +0530

    Card component created
    Card component for customer count, product count etc. was created.

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsFillArchiveFill } from "react-icons/bs";
+import "../css/card.css";
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="main-cards">
+        <div className="card">
+          <div className="card-inner">
+            <h3>PRODUCTS</h3>
+            <BsFillArchiveFill className="card_icon" />
+          </div>
+          <h1>300</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
