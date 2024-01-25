5dec265778339376937a3c38a2ef5f991a3309e7
Thisara Nilupul <thisaranilupul99@gmail.com>
2024-01-25 05:39:43 +0530

    Client-List Component

---
+++ src/Components/ClientList-Component/ClientList.jsx
@@ -0,0 +1,171 @@
+import React, {useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Table,
+    TableBody, 
+    TableCell, 
+    TableContainer, 
+    TableHead, 
+    TableRow,
+    Paper,
+    Avatar,
+    Grid,
+    Typography,
+    TablePagination, 
+    TableFooter,
+    TextField,
+    Button } from '@material-ui/core';
+
+
+
+const useStyles = makeStyles((theme) => ({
+    table: {
+      minWidth: 650,
+    },
+    TableContainer: {
+        borderRadius: 15,
+        margin: '20px 30px 10px 30px',
+        maxWidth: 1200
+    },
+    tableHeaderCell: {
+        fontWeight: 'bold',
+        backgroundColor: theme.palette.primary.dark,
+        color: theme.palette.getContrastText(theme.palette.primary.dark),
+    },
+    Avatar: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.getContrastText(theme.palette.primary.light),
+    },
+    name: {
+        fontWeight: 'bold',
+        color: theme.palette.secondary.dark,
+    },
+    TextField: {
+        margin: '15px 30px 5px 80px',
+        width: 400,
+    },
+    Button: {
+        margin: '30px 30px 0 230px',
+        width: 200,
+        borderRadius: 5,
+    },
+    div: {
+        maxWidth: 1200,
+        display: 'flex',
+    }
+
+  }));
+
+let USERS = [], STATUES = ['Active', 'Inactive'];
+for(let i=0;i<14;i++){
+    USERS[i] = {
+        name: `User${i + 1}`,
+        email: `user${i + 1}@example.com`,
+        phone: `123-456-789${i}`,
+        vehicleID:`V${i + 1}`,
+        vehicleName: `Vehicle ${i + 1}`,
+        joinDate: new Date().toLocaleDateString(),
+        status: STATUES[Math.floor(Math.random() * STATUES.length)]
+    }
+}
+
+function ClientList() {
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [searchName, setSearchName] = useState('');
+    const [searchVehicleID, setSearchVehicleID] = useState('');
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
+
+    const handleNameChange = (event) => {
+        setSearchName(event.target.value);
+    };
+    
+    const handleVehicleIDChange = (event) => {
+        setSearchVehicleID(event.target.value);
+    };
+
+    const filteredUsers = USERS.filter((user) =>
+        user.name.toLowerCase().includes(searchName.toLowerCase()) &&
+        user.vehicleID.toLowerCase().includes(searchVehicleID.toLowerCase())
+    );
+
+
+  return (
+    <div>
+        <div className={classes.div}>
+            <TextField className={classes.TextField}
+                label="Search by Name"
+                value={searchName}
+                onChange={handleNameChange}
+            />
+            <TextField className={classes.TextField}
+                label="Search by Vehicle ID"
+                value={searchVehicleID}
+                onChange={handleVehicleIDChange}
+            />
+            <Button variant="contained" color="primary" className={classes.Button}>
+                New Client
+            </Button>
+        </div>
+           <TableContainer component={Paper} className={classes.TableContainer}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell className={classes.tableHeaderCell}>Client Info</TableCell>
+                    <TableCell className={classes.tableHeaderCell}>Vehicle Info</TableCell>
+                    <TableCell className={classes.tableHeaderCell}>Joining Date</TableCell>
+                    <TableCell className={classes.tableHeaderCell}>Status</TableCell>          
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                    <TableRow key={row.name}>
+                    <TableCell>
+                        <Grid container>
+                            <Grid item lg={2}>
+                                <Avatar alt={row.name} src='.' className={classes.Avatar} />
+                            </Grid>
+                            <Grid item lg={10}>    
+                                <Typography className={classes.name}>{row.name}</Typography>
+                                <Typography color="textSecondary" variant='body2'>{row.email}</Typography>
+                                <Typography color="textSecondary" variant='body2'>{row.phone}</Typography>
+                            </Grid>    
+                        </Grid>
+                    </TableCell>
+                    <TableCell>
+                         <Typography color="primary" variant='subtitle2'>{row.vehicleID}</Typography>
+                         <Typography color="textSecondary" variant='body2'>{row.vehicleName}</Typography>
+                    </TableCell>
+                    <TableCell>{row.joinDate}</TableCell>
+                    <TableCell>{row.status}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+                <TableFooter>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 50, 100]}
+                        component="div"
+                        count={USERS.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    </div>
+  )
+}
+
+export default ClientList
+
