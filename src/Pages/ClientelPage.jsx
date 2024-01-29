b24f580358c4dcfb85cdd946578f71248993d89b
Thisara Nilupul <thisaranilupul99@gmail.com>
2024-01-29 16:46:48 +0530

    add Clientel page

---
+++ src/Pages/ClientelPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClientList from '../Components/ClientList-Component/ClientList'
+
+function ClientelPage() {
+  return (
+    <div>
+      <ClientList/>
+    </div>
+  )
+}
+
+export default ClientelPage
