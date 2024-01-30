e28ed3abeb6990099d1842d42b79f2f61a68d861
Gosith Jayathilake <gosithnalan@gmail.com>
2024-01-30 09:42:39 +0530

    On going services added

--- src/Pages/OnGoingServices/OnGoingServices.jsx
+++ src/Pages/OnGoingServices/OnGoingServices.jsx
@@ -1,50 +1,98 @@
-import React from 'react'
-import Footer from '../../Components/Footer-Component/footerComponent';
-import Header from '../../Components/Header-Component/headerComponent';
-import Sidenav from '../../Components/Side Nav/sideNav';
-import Card from '@mui/material/Card';
+import React from 'react';
+import CardContent from '@mui/material/CardContent';
 import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import Button from '@mui/material/Button';
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
 }));
 
-function OnGoingServices(props) {
-    const [data, setData] = useState([]);
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/service/')
-            .then(response => {
-                setData(response.data);
-                console.log(response.data)
+var cardStyle = {
+    
+    
+    transitionDuration: '0.3s',
+    height: '14vw'
+};
 
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
-    return (
-        <div>
-            {/* <Header position="sticky"></Header> */}
-         <Box>
-           Services
-         </Box>
-        </div>
-    )
+function OnGoingServices() {
+  return (
+    <div>
+
+      <Grid container spacing={2} sx={{
+        padding: '20px',
+      }} >
+        <Grid item xs={4}>
+          <Item > <CardContent  style={cardStyle} sx={{
+            padding: '20px',
+          }}>
+            <Typography sx={{ fontSize: 91 }} color="text.secondary" gutterBottom>
+              <Button sx={{ "&:hover": { backgroundColor: "transparent" }} } >
+               <Typography  sx={{ fontSize: 91 }}>
+                +
+               </Typography>
+              </Button>
+            </Typography>
+            
+           
+          </CardContent></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item> <CardContent  style={cardStyle}>
+            <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
+              Service
+            </Typography>
+            <Typography variant="h5" component="div">
+              Pending at 100000km
+            </Typography>
+           
+          </CardContent></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item> <CardContent  style={cardStyle}>
+            <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
+              Body Wash
+            </Typography>
+            <Typography variant="h5" component="div">
+              Pending at 100000km
+            </Typography>
+            <Button  >
+                <Typography>
+                    Remove
+                </Typography>
+            </Button>
+          </CardContent></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item> <CardContent style={cardStyle}>
+            <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
+              Under Carriage wash
+            </Typography>
+            <Typography variant="h5" component="div">
+              Pending at 100000km
+            </Typography>
+           
+          </CardContent></Item>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default OnGoingServices
--- src/Pages/Repairs/RepairPage.jsx
+++ src/Pages/Repairs/RepairPage.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
-import Footer from '../../Components/Footer-Component/footerComponent';
-import Header from '../../Components/Header-Component/headerComponent';
-import Sidenav from '../../Components/Side Nav/sideNav'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
 
 function RepairPage() {
   return (
     <div>
 
-         <Box>
-            Repairs
-         </Box>
-           
-            
+      
     </div>
   )
 }
