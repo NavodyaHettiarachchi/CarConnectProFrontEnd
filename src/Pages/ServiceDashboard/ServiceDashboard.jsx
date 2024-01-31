95a2a7b54ccef240d72d98771ac6b1c09bda97a6
Navodya Hettiarachchi <navo.hetti@gmail.com>
2024-01-31 11:46:17 +0530

    WIP

---
+++ src/Pages/ServiceDashboard/ServiceDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServiceComponent from '../../Components/Service-Component/ServiceComp';
+
+function ServiceDashboardPage() { 
+  return (
+    <div>
+      <ServiceComponent />
+    </div>
+  )
+}
+
+export default ServiceDashboardPage;
