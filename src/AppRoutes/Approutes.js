99cccabbd9b2ec0dcd2284bdda427b81e357f3d3
Sandun Subhashana <sandunsub2560@gmail.com>
2024-02-22 08:52:48 +0530

    Service dashboard done

--- src/AppRoutes/Approutes.js
+++ src/AppRoutes/Approutes.js
@@ -9,19 +9,18 @@ import RegisterPage from "../Pages/Register/RegisterPage";
 import CenterAdmin from "../Pages/CenterAdminPage/CenterAdminPage";
 import Employees from "../Pages/EmployeePage/ViewEmployeePage";
 import Vehicle from "../Pages/VehiclePage/Vehicle";
+import ServiceHome from "../Pages/ServiceHome/ServiceHome";
 
-
-export const AppRoutes={
-   
-    "login":{ path: "/login", component: (<LoginPage />)},
-    "signup":{ path: "/signup", component: (<RegisterPage />)},
-    "ongoing_services": { path: "/service/", component: (<OnGoingServices/>)},
-    "repairs": { path: "/repairs/", component: (<RepairPage />)},
-    "add_service": { path: "/addservice/", component: (<AddServicePage />)},
-    "view_clients": { path: "/clientel/", component: (<ViewClientel />)},
-    "inventory": { path: "/inventory", component: <Inventory /> },
-    "admin": { path: "/admin", component: <CenterAdmin /> },
-    "view_employee": { path: "/employee", component: <Employees /> },
-    "view_vehicle": { path: "/vehicle", component: <Vehicle /> },
-}
-
+export const AppRoutes = {
+  login: { path: "/login", component: <LoginPage /> },
+  signup: { path: "/signup", component: <RegisterPage /> },
+  ongoing_services: { path: "/service/", component: <OnGoingServices /> },
+  repairs: { path: "/repairs/", component: <RepairPage /> },
+  add_service: { path: "/addservice/", component: <AddServicePage /> },
+  view_clients: { path: "/clientel/", component: <ViewClientel /> },
+  inventory: { path: "/inventory", component: <Inventory /> },
+  admin: { path: "/admin", component: <CenterAdmin /> },
+  view_employee: { path: "/employee", component: <Employees /> },
+  view_vehicle: { path: "/vehicle", component: <Vehicle /> },
+  home: { path: "/", component: <ServiceHome /> },
+};
