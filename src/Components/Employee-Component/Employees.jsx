059c603049ab48fb1a6ebce3cbbc72b453e0afef
Navodya Hettiarachchi <navo.hetti@gmail.com>
2024-02-29 22:43:26 +0530

    need to change lol

--- src/Components/Employee-Component/Employees.jsx
+++ src/Components/Employee-Component/Employees.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Employees.css'
 import {
   Table,
@@ -19,29 +19,27 @@ import {
   Button
 } from '@mui/material'
 import Headerfile from '../../Components/Page-Header/CardHeader';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
-//sample data set
-let EMPLOYEES = [];
-for (let i = 0; i < 14; i++) {
-  EMPLOYEES[i] = {
-    name: `Employee${i + 1}`,
-    email: `employee${i + 1}@example.com`,
-    phone: `123-456-789${i}`,
-    empID: `Emp${i + 1}`,
-    designation: `Mechanic ${i + 1}`,
-    joinDate: new Date().toLocaleDateString(),
-  }
-}
-
-
-
+const columns = [
+  { id: 'profile_pic', label: '', minWidth: 50, maxWidth: 50 },
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'email', label: 'Email', minWidth: 170 },
+  { id: 'contact', label: 'Phone', minWidth: 170 },
+  { id: 'nic', label: 'NIC', minWidth: 170 },
+  { id: 'manager', label: 'Manager', minWidth: 400, format: (value) => value.manager_name.toString() + ', ' + value.manager_designation.toString() },
+  { id: 'isActive', label: 'Status', minWidth: 80, maxWidth: 80 }
+]
 
 function Employees() {
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(6);
   const [searchName, setSearchName] = useState('');
-  const [searchEmpID, setSearchEmpID] = useState('');
+  const [searchEmpNIC, setSearchEmpNIC] = useState('');
+  const [empData, setEmpData] = useState([]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -56,84 +54,96 @@ function Employees() {
     setSearchName(event.target.value);
   };
 
-  const handleEmpIDChange = (event) => {
-    setSearchEmpID(event.target.value);
+  const handleEmpNICChange = (event) => {
+    setSearchEmpNIC(event.target.value);
   };
 
-  const filteredEmployees = EMPLOYEES.filter((user) =>
+  useEffect(() => {
+    fetch("http://localhost:5000/center/getemployee", {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        schema: 'service_pqr_service_center',
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setEmpData(data.data.empData);
+      })
+      .catch((error => { console.log(error) }));
+  }, []);
+
+  let filteredEmployees = empData.filter((user) =>
     user.name.toLowerCase().includes(searchName.toLowerCase()) &&
-    user.empID.toLowerCase().includes(searchEmpID.toLowerCase())
+    user.nic.toLowerCase().includes(searchEmpNIC.toLocaleLowerCase())
   );
 
   return (
     <div>
       <Headerfile title="Employees" />
-      <Card>
+      <Card sx={{ height: '80vh' }}>
         <CardContent>
-        <div className='div' style={{ textAlign: "center", marginTop: 30 }}>
-          <TextField sx={{ marginRight: 20, marginLeft: 5, width: 200 }} className='TextField1'
-            id="standard-basic"
-            variant="standard"
-            label="Search by Employee Name"
-            value={searchName}
-            onChange={handleNameChange}
-          />
-          <TextField sx={{ marginRight: 20, marginLeft: 5, width: 200 }} className='TextField1'
-            id="standard-basic"
-            variant="standard"
-            label="Search by Employee ID"
-            value={searchEmpID}
-            onChange={handleEmpIDChange}
-          />
-          <Button variant="contained" color="primary" className='Button' sx={{ right: 0, left: 'auto' }}>
-            New Employee
-          </Button>
-        </div>
-        <TableContainer component={Paper} className='TableContainer' sx={{ width: '95%' }}>
-          <Table className='table' aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell className='tableHeaderCell' style={{ backgroundColor: '#2222', fontWeight: 'bold' }}>Employee Name/Email/PhoneNo</TableCell>
-                <TableCell className='tableHeaderCell' style={{ backgroundColor: '#2222', fontWeight: 'bold' }}>Employee Id/Designation</TableCell>
-                <TableCell className='tableHeaderCell' style={{ backgroundColor: '#2222', fontWeight: 'bold' }}>Joining Date</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredEmployees.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell>
-                    <Grid container>
-                      <Grid item lg={2}>
-                        <Avatar alt={row.name} src='.' className='Avatar' />
-                      </Grid>
-                      <Grid item lg={10}>
-                        <Typography className='name'>{row.name}</Typography>
-                        <Typography color="textSecondary" variant='body2' >{row.email}</Typography>
-                        <Typography color="textSecondary" variant='body2'>{row.phone}</Typography>
-                      </Grid>
-                    </Grid>
-                  </TableCell>
-                  <TableCell>
-                    <Typography color="primary" variant='subtitle2'>{row.empID}</Typography>
-                    <Typography color="textSecondary" variant='body2'>{row.designation}</Typography>
-                  </TableCell>
-                  <TableCell>{row.joinDate}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TablePagination
-                rowsPerPageOptions={[6, 10, 50, 100]}
-                component="div"
-                count={EMPLOYEES.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </TableFooter>
-          </Table>
-          </TableContainer>
+          <div className='div' style={{ textAlign: "center", marginTop: 30 }}>
+            <TextField sx={{ marginRight: 20, marginLeft: 5, width: 200 }} className='TextField1'
+              id="standard-basic"
+              variant="standard"
+              label="Search by Employee Name"
+              value={searchName}
+              onChange={handleNameChange}
+            />
+            <TextField sx={{ marginRight: 20, marginLeft: 5, width: 200 }} className='TextField1'
+              id="standard-basic"
+              variant="standard"
+              label="Search by Employee NIC"
+              value={searchEmpNIC}
+              onChange={handleEmpNICChange}
+            />
+            <Button variant="contained" color="primary" className='Button' sx={{ right: 0, left: 'auto' }}>
+              New Employee
+            </Button>
+          </div>
+          <div style={{ overflowX: 'auto', position: 'relative' }}>
+            <TableContainer component={Paper} sx={{ height: '65vh', minWidth: '1600px', marginTop: '1%' }} style={{ overflowX: 'auto' }}>
+              <Table stickyHeader>
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth, maxWidth: column.maxWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {filteredEmployees.map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          console.log('Row : ', row);
+                          return (
+                            column.id !== 'isActive' ? (<TableCell key={column.id} align={column.align}>
+                              {column.format && row.manager_id !== null ? column.format(row) : value}
+                            </TableCell>) : <TableCell key={column.id} align={column.align}>
+                              <FormGroup>
+                                <FormControlLabel disabled control={<Switch />} checked={value} label={value ? 'Active' : 'Inactive'} />
+                              </FormGroup>
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
         </CardContent>
       </Card>
     </div>
