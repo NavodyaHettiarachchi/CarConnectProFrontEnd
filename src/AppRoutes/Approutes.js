56f1d8b11511adfae41c5eeded9647f1cf50dbc4
Navodya Hettiarachchi <navo.hetti@gmail.com>
2024-04-17 08:54:04 +0530

    changes

--- src/AppRoutes/Approutes.js
+++ src/AppRoutes/Approutes.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import ViewClientel from "../Components/Clientel-Component/ViewClientel";
-import OnGoingServices from "../Pages/OnGoingServices/OnGoingServices";
+// import OnGoingServices from "../Pages/OnGoingServices/OnGoingServices";
 import RepairPage from "../Pages/Repairs/RepairPage";
 import AddServicePage from "../Pages/AddService/AddService";
 import Inventory from "../Components/Inventory/Inventory";
@@ -9,38 +9,40 @@ import LoginPage from "../Components/Login/Login";
 import RegisterPage from "../Pages/Register/RegisterPage";
 import CenterAdmin from "../Pages/CenterAdminPage/CenterAdminPage";
 import Employees from "../Pages/EmployeePage/ViewEmployeePage";
-// import Vehicle from "../Pages/VehiclePage/VehicleCard";
-// import Vehicle from "../Pages/VehiclePage/VehicleCard";
-import Feedback from "../Pages/FeedbackPage/Feedback";
-import { useState } from "react";
-import ServiceHome from "../Pages/ServiceHome/ServiceHome";
 
-import Vehicle from "../Pages/VehiclePage/VehicleCard";
-
+// import Vehicle from "../Pages/VehiclePage/VehicleCard";
 // import Feedback from "../Pages/FeedbackPage/Feedback";
 // import { useState } from "react";
 // import ServiceHome from "../Pages/ServiceHome/ServiceHome";
+
+import Vehicle from "../Pages/VehiclePage/VehicleCard";
+// import Feedback from "../Pages/FeedbackPage/Feedback";
+// import { useState } from "react";
+// import ServiceHome from "../Pages/ServiceHome/ServiceHome";
+
 import EmployeeProfile from "../Pages/Profile/EmployeeProfile";
 import CenterProfile from "../Pages/Profile/CenterProfile";
 import OwnerProfile from "../Pages/Profile/OwnerProfie";
 import AdminProfile from "../Pages/Profile/AdminProfile";
+import OnGoingServicesNew from "../Pages/OnGoingServices/OngoingServicesNew";
 
-export const AppRoutes={
-
+export const AppRoutes = {
     // "home": { path: "/", component: (islogged?  null: <Feedback/> )},
-    "login": { path: "/login/", component: <LoginPage />},
-    "signup":{ path: "/signup", component: (<RegisterPage />)},
-    "ongoing_services": { path: "/service/", component: (<OnGoingServices/>)},
-    "repairs": { path: "/repairs/", component: (<RepairPage />)},
-    "add_service": { path: "/addservice/", component: (<AddServicePage />)},
-    "view_clients": { path: "/clientel/", component: (<ViewClientel />)},
-    "inventory": { path: "/inventory", component: <Inventory /> },
-    "admin": { path: "/admin", component: <CenterAdmin /> },
-    "view_employee": { path: "/employee", component: <Employees /> },
-    "view_vehicle": { path: "/vehicle", component: <Vehicle /> },
-    "EmployeeProfile": { path: "/employee/profile", component: < EmployeeProfile/> },
-    "CenterProfile": { path: "/center/profile", component: <CenterProfile/> },
-    "OwnerProfile": { path: "/owner/profile", component: <OwnerProfile/> },
-    "AdminProfile": { path: "/admin/profile", component: <AdminProfile/> },
-    
-}
+    login: { path: "/login", component: <LoginPage /> },
+    signup: { path: "/signup", component: <RegisterPage /> },
+    ongoing_services: { path: "/service/", component: <OnGoingServicesNew /> },
+    repairs: { path: "/repairs/", component: <RepairPage /> },
+    add_service: { path: "/addservice/", component: <AddServicePage /> },
+    view_clients: { path: "/clientel/", component: <ViewClientel /> },
+    inventory: { path: "/inventory", component: <Inventory /> },
+    admin: { path: "/admin", component: <CenterAdmin /> },
+    view_employee: { path: "/employee", component: <Employees /> },
+    view_vehicle: { path: "/vehicle", component: <Vehicle /> },
+    EmployeeProfile: {
+        path: "/employee/profile",
+        component: <EmployeeProfile />,
+    },
+    CenterProfile: { path: "/center/profile", component: <CenterProfile /> },
+    OwnerProfile: { path: "/owner/profile", component: <OwnerProfile /> },
+    AdminProfile: { path: "/admin/profile", component: <AdminProfile /> },
+};
