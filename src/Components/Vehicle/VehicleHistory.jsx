16d9dca720cee0694141ab1543cd77a33f649916
Thisara Nilupul <thisaranilupul99@gmail.com>
2024-04-20 06:25:57 +0530

    history view and filter

--- src/Components/Vehicle/VehicleHistory.jsx
+++ src/Components/Vehicle/VehicleHistory.jsx
@@ -5,16 +5,32 @@ import {
   DialogTitle,
   IconButton,
   Grid,
-  Typography
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
 } from '@mui/material';
 import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from '@mui/icons-material/Menu';
 import axios from 'axios';
 import HistoryFilter from './VehicleHistoryFilter';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 
 function VehicleHistory({ open, vehicleId, closeVehicleHistory }) {
   //vehicle State
   const [vehicleHistory, setVehicleHistory] = useState([]);
+  const [filteredHistory, setFilteredHistory] = useState([]);
+  
   const [vehicleData, setVehicleData] = useState({
     vehicle_id: '',
     number_plate: '',
@@ -25,6 +41,23 @@ function VehicleHistory({ open, vehicleId, closeVehicleHistory }) {
   });
   // filter State
   const [openFilter, setOpenFilter] = useState(false);
+  const [openm, setOpenm] = useState(null);
+
+  const handleFilteredHistory = (filteredHistoryData) => {
+    // Update state with filtered history data
+    setFilteredHistory(filteredHistoryData);
+    
+  };
+
+  const handleFilterCancel = () => {
+    // Reset filtered history to original history
+    setFilteredHistory(vehicleHistory);
+  };
+
+  const handleYearClick = (year) => {
+    setOpenm((prevYear) => (prevYear === year ? null : year));
+  }
+
 
   const filtersGiven = [
     { id: 'f_date', type: 'date', label: 'From Date', description: 'Filter history from this date' },
@@ -81,6 +114,27 @@ function VehicleHistory({ open, vehicleId, closeVehicleHistory }) {
     });
     setVehicleHistory([]);
     closeVehicleHistory();
+  };
+
+  const handleYearConversion = (date) => {
+    return date.split('-')[0];
+  }
+
+  const groupByYear = () => {
+    console.log("filteredVehicleHistory: ", filteredHistory);
+    const historyToGroup = filteredHistory.length > 0 ? filteredHistory : vehicleHistory;
+    const groups = {};
+    
+    historyToGroup.forEach((service, index) => {
+      const year = handleYearConversion(service.service_date);
+      if (!groups[year]) {
+        groups[year] = [];
+      }
+      groups[year].push({ ...service, index });
+    });
+
+    // console.log("group ", groups);
+    return groups;
   };
 
   const handleDateConversions = (date) => {
@@ -143,13 +197,78 @@ function VehicleHistory({ open, vehicleId, closeVehicleHistory }) {
               </Grid>
             </Grid>
           </div>
+          <div>
+            <List
+              sx={{width: '95%', minWidth: 400, marginTop: 5, marginLeft: 6, bgcolor: 'background.paper' }}
+              component="nav"
+              aria-labelledby="service-history-subheader"
+              subheader={
+                <Typography variant="h6" gutterBottom component="div" id="service-history-subheader" sx={{marginBottom: 2 }}>
+                  Service History
+                </Typography>
+              }
+            >
+               {Object.entries(groupByYear()).map(([year, services]) => (
+                <React.Fragment key={year}>
+                  <ListItemButton onClick={() => handleYearClick(year)}>
+                    <ListItemText primary={`Year: ${year}`} />
+                    {openm === year ? <ExpandLess /> : <ExpandMore />}
+                  </ListItemButton>
+                  <Collapse in={openm === year} timeout="auto" unmountOnExit sx={{marginBottom: 2}}>
+                    <List disablePadding>
+                      <div style={{ overflowX: 'auto', position: 'relative' }}>
+                        <TableContainer component={Paper} sx={{ minWidth: 400, marginTop: '1%' }} style={{ overflowX: 'auto' }}>
+                          <Table stickyHeader>
+                            <TableHead component={Paper}>
+                              <TableRow>
+                                <TableCell >
+                                  Service Date
+                                </TableCell>
+                                <TableCell  >
+                                  Description
+                                </TableCell>
+                                <TableCell  >
+                                  Mileage
+                                </TableCell>
+                                <TableCell  >
+                                  Cost: Rs.
+                                </TableCell>
+                              </TableRow>
+                            </TableHead>
+                            <TableBody>
+                              {services.map((service) => (
+                                <TableRow key={service.index}>
+                                  <TableCell>{handleDateConversions(service.service_date)}</TableCell>
+                                  <TableCell>{service.description}</TableCell>
+                                  <TableCell>{service.mileage}</TableCell>
+                                  <TableCell>{`Rs. ${parseFloat(service.cost).toFixed(2)}`}</TableCell>
+                                  <TableCell>
+                                    <IconButton aria-label="delete" sx={{fontSize: 'small'}} >
+                                      Download Invoice <DownloadForOfflineIcon sx={{marginLeft: 1}} color="primary"/>
+                                    </IconButton>
+                                  </TableCell>
+                                </TableRow>
+                              ))}
+                            </TableBody>
+                          </Table>
+                        </TableContainer>
+                      </div>
+                    </List>
+                  </Collapse>
+                </React.Fragment>
+              ))}
+            </List>
+          </div>
+
         </DialogContent>
       </Dialog>
 
+        
       <HistoryFilter
         openFilter={openFilter}
+        vehicleId={vehicleId}
         vehicleData={vehicleData}
-        setParentVehicleData={() => { console.log('update'); }}
+        onFilteredHistory={handleFilteredHistory}
         closeFilterPopup={() => setOpenFilter(false)}
       />
     </div>
